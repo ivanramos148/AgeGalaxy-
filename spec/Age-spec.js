d2efc0a1ec9ddfda06d934249082b958ad2bb5a4
Guest User <Guest@Epicodus-6B.local>
2018-03-19 13:10:25 -0700

    remove age constructor to add to commit

--- spec/Age-spec.js
+++ spec/Age-spec.js
@@ -1,10 +1,7 @@
  import { Age } from './../src/Age.js'
 
 describe('Age', function() {
-  it('should take the users age and make in into second', function() {
-    let age = new Age(20)
-    expect(age.yearToSeconds()).toEqual(Math.round(31557459.167*20));
-  });
+
   it('Take two dates and determine the difference, in seconds, between the two', function() {
     let age = new Age(20);
     let Date1 = new Date(1995, 10, 23);
--- src/Age.js
+++ src/Age.js
@@ -1,7 +1,4 @@
-export class Age {
-  constructor(age) {
-    this.age = age;
-  }
+
   yearToSeconds(){
     const toSeconds = 31557459.167;
     let userAge = this.age;
