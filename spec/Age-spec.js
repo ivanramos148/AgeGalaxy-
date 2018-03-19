54d79e5411b831d440fc61aa8f120d00f80cdbca
Guest User <Guest@Epicodus-6B.local>
2018-03-19 13:05:35 -0700

    add dates that compares two dates

--- spec/Age-spec.js
+++ spec/Age-spec.js
@@ -5,11 +5,12 @@ describe('Age', function() {
     let age = new Age(20)
     expect(age.yearToSeconds()).toEqual(Math.round(31557459.167*20));
   });
-    it('Take two dates and determine the difference, in seconds, between the two', function() {
-      let age = new Age(20)
-      let date1 = new Date(1995, 10, 23);
-      let date2 = new Date(1997, 10, 5);
-      console.log(date2.getTime());
-      expect(age.differenceInSeconds(date1, date2)).toEqual();
-    });
+  it('Take two dates and determine the difference, in seconds, between the two', function() {
+    let age = new Age(20);
+    let Date1 = new Date(1995, 10, 23);
+    let Date2 = new Date(1997, 10, 5);
+    let difference = Date1.getTime() - Date2.getTime();
+    console.log(difference);
+    expect(age.differenceInSeconds(Date1, Date2)).toEqual(difference);
+  });
 });
--- src/Age.js
+++ src/Age.js
@@ -1,19 +1,16 @@
 export class Age {
   constructor(age) {
     this.age = age;
-
   }
-
   yearToSeconds(){
     const toSeconds = 31557459.167;
     let userAge = this.age;
     let galaxyAge = Math.round(userAge * toSeconds);
     return galaxyAge;
   }
-  //
-  // venusAge(){
-  //   let venusYears = 2;
-  //   let venusResult = Math.round(galaxyAge / venusYears);
-  //   return galaxyAge;
-  // }
-}
+  differenceInSeconds(date1, date2){
+    let date1Seconds = date1.getTime();
+    let date2Seconds = date2.getTime();
+    let difference  = date1Seconds - date2Seconds;
+    return difference;
+  }
